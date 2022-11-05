3eefb6408007ecdcfe1e3fc904aceedb980a7163
Erik-Kan <43632803+Erik-Kan@users.noreply.github.com>
2022-11-04 22:53:04 -0700

    Fixed misplaces tile search and added manhattan search

--- Prog.py
+++ Prog.py
@@ -6,12 +6,15 @@ import copy
 # https://docs.python.org/3/library/time.htm
 # https://docs.python.org/3/tutorial/classes.html
 # https://www.geeksforgeeks.org/copy-python-deep-copy-shallow-copy/
+#https://www.geeksforgeeks.org/priority-queue-in-python/
+
 
 class Node:
     def __init__(self, depth, board):
         self.depth = depth
         self.board = board
         self.cost = 0
+        self.heuristic = 0
         self.childU = None
         self.childD = None
         self.childL = None
@@ -24,11 +27,11 @@ def main():
 goalState = [['1','2','3'], ['4','5','6'], ['7','8','0']]
 #board = [[], [], []]
 
-
+#print board function
 def printBoard(board):
     for i in range(len(board)):
 
-        for j in range(len(board[i])):
+        for j in range(len(board)):
             
             print(board[i][j], end = ' ')
 
@@ -38,14 +41,14 @@ def printBoard(board):
 def getChilds(Node1):
     for i in range(len(Node1.board)):
 
-        for j in range(len(Node1.board[i])):
+        for j in range(len(Node1.board)):
            # print(i)
            # print(j)
             #print(Node.board[i][j])
             if Node1.board[i][j] == "0":
                 #print("hi5")
                 #print(j)
-                if i > 0:
+                if i > 0:#checks if a child in able to be create in each direction and if so it creates a copy of the current node and uses it to make a child
                     childBoard = copy.deepcopy(Node1.board)
                     childBoard[i][j] = childBoard[i-1][j]
                     childBoard[i-1][j] = Node1.board[i][j]
@@ -72,7 +75,7 @@ def getChilds(Node1):
 
 
 
-def boardDecision():
+def boardDecision():#decision to choose what type of board you create
     Pass = False
     while Pass == False:
         choice = input(" choose '0' to create your own board or choose one of the 8 other board u want to solve (1-8): ")
@@ -129,7 +132,7 @@ def boardDecision():
 
 
 
-def searchDecision(board):
+def searchDecision(board):#chooses what type of search function you want to run on your board
     choice2 = input('Which search would you like to implement? Options: Uniform Cost Search, Misplaced Tile Search, Manhatten Distance Search (1,2,3) ')
     
     
@@ -144,21 +147,22 @@ def searchDecision(board):
         
     elif choice2 == '2':
         print('Misplaced Tile Search chosen')
-        #MisTileSearch(board, goalState)
+        
         UniformCostSearch(board, goalState, choice2)
         
 
     elif choice2 == '3':
         print('Manhatten Distance Search chosen')
-        ManhattenDistSearch(board, goalState)
+        UniformCostSearch(board, goalState, choice2)
+        
        
     
 
 
-#Breadth First Search - A* g(n) + h(n) where h(n) = 0
 def UniformCostSearch(board,goalState, choice2):
-    lowest = 100
-    
+#Breadth First Search - A* g(n) + h(n) where h(n) = 0
+    #lowest = 100
+    startTime = time.time()
 
    
     maxQSize = 0
@@ -170,18 +174,30 @@ def UniformCostSearch(board,goalState, choice2):
     
     done = []
     
-    #for k in range(0,30):
+    #for k in range(0,35):
     while len(queue) > 0 :
         MisChild1= 100
         MisChild2= 100
         MisChild3= 100
         MisChild4= 100
         lowestChild = 0
-        i = queue.pop(0)
-        print("hi2")
+        lowest = 100
+        
+        minCostChild = 0
+        for e in range (len(queue)):#prioity queue where the lowest cost nodes get popped first
+            if queue[e].cost < queue[minCostChild].cost:
+                minCostChild = e
+        i = queue.pop(minCostChild)
+        print("The best state to expand with a g(n) =" + str(i.depth) + "and h(n) =" + str(i.heuristic))
+        NumNodesExpanded+=1
+        
+
+        #print("hi2")
         printBoard(i.board)
-        if i.board == goalState:
+        if i.board == goalState:#output once goal state is achieved
+            print("\n)")
             printBoard(i.board)
+            print("Time elapsed : " + str(f'{(time.time()- startTime):.2f}'))#gets the time elapsed
             print ("goal State!")
             print ("solution depth was: " + str(i.depth))
             print ("Number of Nodes expanded: " + str(NumNodesExpanded) )
@@ -189,54 +205,68 @@ def UniformCostSearch(board,goalState, choice2):
             return
        
         getChilds(i)
-        #printBoard(i.board)
-       # print("hi")
-        #print(i.childU)
-        if done == []:
+        
+
+        if done == []:#runs if it is the first node and there is no chance of a duplicate node
             if i.childU != None:
                 if choice2 =="1":
-                    print("hi4")
+                    #print("hi4")
+                    i.childU.cost = i.childU.depth 
                     queue.append(i.childU)
-                    NumNodesExpanded+=1
+                    #NumNodesExpanded+=1
                 elif choice2 == "2":
-                    print("mis3")
-                    MisChild1  = MisTileSearch(i.childU.board, goalState)
-                    if MisChild1 < lowest:
-                        lowest = MisChild1
-                        
-                        lowestChild = 1
+                    #print("mis3")
+                    i.childU.heuristic  = MisTileSearch(i.childU.board, goalState)
+                    i.childU.cost = i.childU.depth + i.childU.heuristic
+                    queue.append(i.childU)
+                elif choice2 == "3":
+                    i.childU.heuristic  = ManhattenDistSearch(i.childU.board, goalState)
+                    i.childU.cost = i.childU.depth + i.childU.heuristic
+                    queue.append(i.childU)
             if i.childD != None:
                 if choice2 =="1":
+                    i.childD.cost = i.childD.depth 
                     queue.append(i.childD)
-                    NumNodesExpanded+=1
+                    #NumNodesExpanded+=1
                 elif choice2 == "2":
-                    MisChild2 = MisTileSearch(i.childD.board, goalState)
-                    if MisChild2 < lowest:
-                        lowest = MisChild2
-                        lowestChild = 2
+                    i.childD.heuristic = MisTileSearch(i.childD.board, goalState)
+                    i.childD.cost = i.childD.depth + i.childD.heuristic
+                    queue.append(i.childD)
+                elif choice2 == "3":
+                    i.childD.heuristic  = ManhattenDistSearch(i.childD.board, goalState)
+                    i.childD.cost = i.childD.depth + i.childD.heuristic
+                    queue.append(i.childD)
             if i.childR != None:
-                print("mis4")
+                #print("mis4")
                 if choice2 =="1":
+                    i.childR.cost = i.childR.depth 
                     queue.append(i.childR)
-                    NumNodesExpanded+=1
-                elif choice2 == "2":
-                    MisChild3= MisTileSearch(i.childR.board, goalState)
-                    if MisChild3 < lowest:
-                        print("mis5")
-                        lowest = MisChild3
-                        lowestChild = 3
+                    #NumNodesExpanded+=1
+                elif choice2 == "3":
+                    i.childR.heuristic  = ManhattenDistSearch(i.childR.board, goalState)
+                    i.childR.cost = i.childR.depth + i.childR.heuristic
+                    queue.append(i.childR)
+                elif choice2 == "3":
+                    i.childR.heuristic  = ManhattenDistSearch(i.childR.board, goalState)
+                    i.childR.cost = i.childR.depth + i.childR.heuristic
+                    queue.append(i.childR)
             if i.childL != None:
                 if choice2 =="1":
+                    i.childL.cost = i.childL.depth 
                     queue.append(i.childL)
-                    NumNodesExpanded+=1
+                    #NumNodesExpanded+=1
                 elif choice2 == "2":
-                    MisChild4 =  MisTileSearch(i.childL.board, goalState)
-                    if MisChild4 < lowest:
-                        lowest = MisChild4
-                        lowestChild = 4
+                    i.childL.heuristic =  MisTileSearch(i.childL.board, goalState)
+                    i.childL.cost = i.childL.depth + i.childL.heuristic
+                    queue.append(i.childL)
+                elif choice2 == "3":
+                    i.childL.heuristic  = ManhattenDistSearch(i.childL.board, goalState)
+                    i.childL.cost = i.childL.depth + i.childL.heuristic
+                    queue.append(i.childL)
 
         else:
             #print(done)
+            #checks for duplicate nodes in the done array
             dupU = False
             dupD = False
             dupR = False
@@ -250,102 +280,75 @@ def UniformCostSearch(board,goalState, choice2):
                     dupR = True
                 if i.childL != None and j.board == i.childL.board:
                     dupL = True
-            if i.childU != None and dupU == False :
-                print("hi6")
+            if i.childU != None and dupU == False :#if up child was created it runs it through the search chosen above
+                #print("hi6")
                 if choice2 =="1":
-                    print("hi7")
+                    #print("hi7")
+                    i.childU.cost = i.childU.depth
                     queue.append(i.childU)
-                    NumNodesExpanded+=1
-                elif choice2 == "2":
-                    MisChild1  = MisTileSearch(i.childU.board, goalState)
-                    if MisChild1 < lowest:
-                        lowest = MisChild1
-                        lowestChild = 1
+                    #NumNodesExpanded+=1
+                elif choice2 == "2":#misplaced
+                    i.childU.heuristic  = MisTileSearch(i.childU.board, goalState)
+                    i.childU.cost = i.childU.depth + i.childU.heuristic
+                    queue.append(i.childU)
+                elif choice2 == "3":#Manhatan distance 
+                    i.childU.heuristic  = ManhattenDistSearch(i.childU.board, goalState)
+                    i.childU.cost = i.childU.depth + i.childU.heuristic
+                    queue.append(i.childU)
                 
-            if i.childD != None and dupD == False:
+            if i.childD != None and dupD == False:#if down child was created it runs it through the search chosen above
                 if choice2 =="1":
+                    i.childD.cost = i.childD.depth
                     queue.append(i.childD)
                     NumNodesExpanded+=1
                 elif choice2 == "2":
-                    MisChild2 = MisTileSearch(i.childD.board, goalState)
-                    if MisChild2 < lowest:
-                        lowest = MisChild2
-                        lowestChild = 2
-
-            if i.childR != None and dupR == False:
-                if choice2 =="1":
-                    queue.append(i.childR)
-                    NumNodesExpanded+=1
-                elif choice2 == "2":
-                    MisChild3= MisTileSearch(i.childR.board, goalState)
-                    if MisChild3 < lowest:
-                        lowest = MisChild3
-                        lowestChild = 3
-                
-            if i.childL != None and dupL == False:
-                if choice2 =="1":
-                    queue.append(i.childL)
-                    NumNodesExpanded+=1
-                elif choice2 == "2":
-                    MisChild4 =  MisTileSearch(i.childL.board, goalState)
-                    if MisChild4 < lowest:
-                        lowest = MisChild4
-                        lowestChild = 4
-
-        if choice2 == "2":
-            print("mis7")
-            if lowestChild ==1:
-                queue.append(i.childU)
-                NumNodesExpanded+=1
-                i.childU.cost = i.childU.depth + lowest
-                if MisChild2 == MisChild1 :
-                    i.childD.cost = i.childD.depth + lowest
+                    i.childD.heuristic = MisTileSearch(i.childD.board, goalState)
+                    i.childD.cost = i.childD.depth + i.childD.heuristic
                     queue.append(i.childD)
-                    NumNodesExpanded+=1
-                if MisChild3 == MisChild1 :
-                    i.childR.cost = i.childR.depth + lowest
+                elif choice2 == "3":
+                    i.childD.heuristic  = ManhattenDistSearch(i.childD.board, goalState)
+                    i.childD.cost = i.childD.depth + i.childD.heuristic
+                    queue.append(i.childD)
+
+            if i.childR != None and dupR == False:#if Right child was created it runs it through the search chosen above
+                if choice2 =="1":
+                    i.childR.cost = i.childR.depth
                     queue.append(i.childR)
-                    NumNodesExpanded+=1
-                if MisChild4 == MisChild1 :
-                    i.childL.cost = i.childL.depth + lowest
-                    queue.append(i.childL)
-                    NumNodesExpanded+=1
-                print("The best state to expand with a g(n) =" + str(i.childU.depth) + "and h(n) =" + str(lowest))
-            elif lowestChild ==2:
-                i.childD.cost = i.childD.depth + lowest
-                queue.append(i.childD)
-                NumNodesExpanded+=1
-                if MisChild3 == MisChild2 :
-                    i.childR.cost = i.childR.depth + lowest
+                    #NumNodesExpanded+=1
+                elif choice2 == "2":
+                    i.childR.heuristic= MisTileSearch(i.childR.board, goalState)
+                    i.childR.cost = i.childR.depth + i.childR.heuristic
                     queue.append(i.childR)
-                    NumNodesExpanded+=1
-                if MisChild4 == MisChild2 :
-                    i.childL.cost = i.childL.depth + lowest
+                elif choice2 == "3":
+                    i.childR.heuristic  = ManhattenDistSearch(i.childR.board, goalState)
+                    i.childR.cost = i.childR.depth + i.childR.heuristic
+                    queue.append(i.childR)
+                
+            if i.childL != None and dupL == False:#if Left child was created it runs it through the search chosen above
+                if choice2 =="1":
+                    i.childL.cost = i.childL.depth
                     queue.append(i.childL)
-                    NumNodesExpanded+=1
-            elif lowestChild ==3:
-                print("mis6")
-                i.childR.cost = i.childR.depth + lowest
-                queue.append(i.childR)
-                NumNodesExpanded+=1
-                if MisChild4 == MisChild3 :
-                    i.childL.cost = i.childL.depth + lowest
+                   # NumNodesExpanded+=1
+                elif choice2 == "2":
+                    i.childL.heuristic =  MisTileSearch(i.childL.board, goalState)
+                    i.childL.cost = i.childL.depth + i.childL.heuristic
                     queue.append(i.childL)
-                    NumNodesExpanded+=1
-            elif lowestChild ==4:
-                i.childL.cost = i.childL.depth + lowest
-                queue.append(i.childL)
-                NumNodesExpanded+=1
-            
+                elif choice2 == "3":
+                    i.childL.heuristic  = ManhattenDistSearch(i.childL.board, goalState)
+                    i.childL.cost = i.childL.depth + i.childL.heuristic
+                    queue.append(i.childL)
+
+      
+                
                 
 
 
                 
 
-        lowest = 100
-        done.append (i)
-        print (len(queue))
-        if maxQSize < len(queue):
+        
+        done.append (i)#adds finished nodes into the done array
+        #print (len(queue))
+        if maxQSize < len(queue):#updates the current max size the queue reaches
             maxQSize = len(queue)
 
     #if board == goal:
@@ -358,22 +361,37 @@ def UniformCostSearch(board,goalState, choice2):
 
 
 def ManhattenDistSearch(board, goalState):
-    ManHatDist = 0
+    
+    MDsum=0
+    for i in range (len(board)):#check whole board
+        for j in range (len(board)):
+            num = int(board [i][j])
+            
+            if num != '0':#ignores 0 tile
+                goalX = (num-1) / (len(board))
+                goalY = (num-1) % (len(board))
+                xDist = i-goalX
+                yDist = j-goalY
+                MDsum += abs(xDist) + abs(yDist)
+
+    ManHatDist = MDsum
+    return ManHatDist
+
     
 
 def MisTileSearch(board, goalState):
-    print('hi Mis')
+    #print('hi Mis')
     
     MisTile =0
-    for i in range (len(board)):
+    for i in range (len(board)):#checks whole board
         for j in range (len(board)):
-            if board [i][j] != '0' and board[i][j] != goalState[i][j] :
+            if board [i][j] != '0' and board[i][j] != goalState[i][j] :#comapres each tile between the goals state and the current state
             
                 MisTile+=1
     #if board[2][2] != '0':
        # MisTile -=1
 
-    print(MisTile)
+    #print(MisTile)
     return MisTile
                 
 
